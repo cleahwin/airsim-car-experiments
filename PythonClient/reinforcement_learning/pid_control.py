4391065d7d420a6790f1d6518f8429b1fc40a54b
cleahwin <cleahwin@gmail.com>
2023-07-26 22:39:38 -0700

    Add several fixes to PID; changed computation to vector format

--- PythonClient/reinforcement_learning/pid_control.py
+++ PythonClient/reinforcement_learning/pid_control.py
@@ -7,9 +7,6 @@ import matplotlib.pyplot as plt
 from scipy.spatial.transform import Rotation as R
 
 
-batch_size = 8
-# estimated y position for car to be straight
-straight_y_pos = 2.26216 * math.pow(10, 5)
 # data set of riding normally through neighborhood
 data_path = "C:/Users/Cleah/Documents/AirSim/2023-05-09-22-09-34/airsim_rec.txt"
 
@@ -28,34 +25,48 @@ for i in range(0, 1000):
     for index, row in df.iterrows():
         car_controls = airsim.CarControls()
         car_state = client.getCarState()
+
         current_y_pos = car_state.kinematics_estimated.position.y_val
         current_x_pos = car_state.kinematics_estimated.position.x_val
+        # Numpy arrays of current x-y position and desired x-y position
+        pos_curr = np.array([[current_x_pos], 
+                              [current_y_pos]])
+        pos_data = np.array([[row["POS_X"]], 
+                              [row["POS_Y"]]])
+
         # current_x_quat = car_state.kinematics_estimated.quaterinion.x_val
         r = R.from_quat([row["Q_X"], row["Q_Y"], row["Q_Z"], row["Q_W"]])
-        r = r.as_euler('xyz')
+        roll, pitch, yaw = r.as_euler('xyz')
 
-        # along track error
-        e_at = (
-           math.sin(r[2]) * (current_x_pos - row["POS_X"]) + 
-            math.cos(r[2]) * (current_y_pos  - row["POS_Y"])
-        )
-        # cross track error
-        e_ct = (
-            -1 * math.sin(r[2]) * (current_x_pos - row["POS_X"]) + 
-            math.cos(r[2]) * (current_y_pos  - row["POS_Y"])
-        )
+        # Position error
+        pos_error = pos_curr - pos_data
+        rotation_matrix = np.array([[math.cos(car_controls.steering), -1 * math.sin(car_controls.steering)],
+                                  [math.sin(car_controls.steering), math.cos(car_controls.steering)]])
+        # first element corresponds to along track error and second to cross track error
+        track_error_matrix = np.matmul(rotation_matrix, pos_error)
+
+        # # along track error
+        # e_at = (
+        #    math.sin(yaw) * (current_x_pos - row["POS_X"]) + 
+        #     math.cos(yaw) * (current_y_pos  - row["POS_Y"])
+        # )
+        # # cross track error
+        # e_ct = (
+        #     -1 * math.sin(yaw) * (current_x_pos - row["POS_X"]) + 
+        #     math.cos(yaw) * (current_y_pos  - row["POS_Y"])
+        # )
         # steering angle error
-        theta_e = car_controls.steering - r[2]
+        theta_e = car_controls.steering - yaw
         k_p = 0.5
         k_d = 1
-        print(f"e_at = {e_at}")
-        print(f"e_ct = {e_ct}")
+        print(f"e_at = {track_error_matrix[0][0]}")
+        print(f"e_ct = {track_error_matrix[1][0]}")
         print(f"theta_e = {theta_e}")
 
         # final calculation
-        u = -1 * (k_p * e_ct + k_d * math.sin(r[0]))
+        u = -1 * (k_p * track_error_matrix[1][0] + k_d * car_state.speed * math.sin(yaw))
         steering_angles.append(u)
-
+        print(f"steering angle {u}")
         # set car controls
         car_controls.throttle = 0.5
         car_controls.steering = u;
